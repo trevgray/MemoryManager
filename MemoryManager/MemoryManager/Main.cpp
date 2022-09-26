c369e726fe57803a4aa9a3cbf03cfef687f62b11
Trev <daeteri360@gmail.com>
2022-09-26 17:46:31 -0400

    Class 9/26/2022

--- MemoryManager/MemoryManager/Main.cpp
+++ MemoryManager/MemoryManager/Main.cpp
@@ -1,18 +1,59 @@
-#include "MemoryManager.h"
 #include <iostream>
 #include <vector>
 
-std::size_t memoryUsed;
+using byte = unsigned char; //basically just 8 bytes
 
-void* operator new(std::size_t amount) {
-	std::cout << "allocating " << amount << " bytes of memory\n";
-	memoryUsed += amount;
-	return malloc(amount);
+struct header_t {
+	std::size_t size;
+	bool isFree; //this block does not get removed, it is just no longer used
+	header_t* next;
+};
+
+class MemoryManager { //https://developer.ibm.com/tutorials/au-memorymanager/
+private:
+	static std::shared_ptr<MemoryManager> instance;
+	byte* masterBlock; //collection of bytes
+	std::size_t blockIterator;
+
+	MemoryManager(std::size_t masterBlockSize) :masterBlock(nullptr) {
+		masterBlock = static_cast<byte*>(malloc(masterBlockSize));
+		blockIterator = 0;
+		std::cout << "Master block is " << masterBlockSize << " bytes" << std::endl;
+	}
+public:
+
+	static std::shared_ptr<MemoryManager> GetInstance() {
+		if (!instance) {
+			instance = std::shared_ptr<MemoryManager>(new MemoryManager(1024));
+		}
+		return instance;
+	}
+	void* allocate(std::size_t memorySize) {
+		void* preIterate = &masterBlock[blockIterator];
+		blockIterator += memorySize;
+		return preIterate;
+		//return &masterBlock[blockIterator += memorySize];
+	}
+	~MemoryManager() {
+		if (masterBlock) free(masterBlock);
+		std::cout << "Freed the MasterBlock" << std::endl;
+	}
+};
+std::shared_ptr<MemoryManager> MemoryManager::instance = 0;
+
+static std::size_t memoryAllocated = 0;
+
+void* operator new(std::size_t memorySize) {
+	if (memorySize == 0) { memorySize++; }
+	std::cout << "allocating " << memorySize << " bytes of memory\n";
+	memoryAllocated += memorySize;
+	return MemoryManager::GetInstance()->allocate(memorySize);
+	//return malloc(memorySize);
 }
 
-void operator delete(void* memoryLocation, std::size_t amount) {
-	std::cout << "freeing " << amount << " bytes of memory\n";
-	memoryUsed -= amount;
+void operator delete(void* memoryLocation, std::size_t memorySize) {
+	std::cout << "freeing " << memorySize << " bytes of memory\n";
+	memoryAllocated -= memorySize;
 	free(memoryLocation);
 }
 
@@ -23,9 +64,8 @@ struct Vec3 {
 	}
 };
 
-int main() {
-	using namespace MEMORYMANAGER;
-	memoryUsed = 0;
+int main(int agrc, char* argv[]) {
+	MemoryManager::GetInstance();
 
 	Vec3* vPtr = new Vec3();
 	delete vPtr;
@@ -38,5 +78,5 @@ int main() {
 		vectors.push_back(Vec3());
 	}
 	vectors.~vector();
-	std::cout << memoryUsed << std::endl;
+	std::cout << memoryAllocated << std::endl;
 }
